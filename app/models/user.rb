bb23ee27383048a743f36ed5004c440516f60698
CestLou <alisonrebeccarobb@gmail.com>
2015-01-23 14:25:24 -0800

    Associations work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
 
-	# has_many :events
+	has_many :events
 	# has_and_belongs_to_many :events, join_table: "attends"
+	# has_many :attends
+	# has_many :attending, through: :attends, source: "Event"
+	has_many :attending, class_name: "Event", join_table: "events"
+	has_many :votes, class_name: "Vote", join_table: "votes"
+
 	# has_many :events, through: :attends, as: :attending
-	has_many :events, through: :attends
 	# belongs_to :events, inverse_of: :attending
 	# has_many :events, through: :votes, as: :votes
 	has_many :tickets
