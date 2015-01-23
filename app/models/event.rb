3e67fdd6c6041900d11191f5ef116a74548d6daa
CestLou <alisonrebeccarobb@gmail.com>
2015-01-23 15:23:26 -0800

    more associations work

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,8 @@ class Event < ActiveRecord::Base
 
   belongs_to :category
   belongs_to :user
-  has_many :votes
-  has_many :attends
+
+	has_and_belongs_to_many :attending, class_name: "User", join_table: "attends"
+	has_and_belongs_to_many :votes, class_name: "User", join_table: "votes"
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,9 @@
 class User < ActiveRecord::Base
 
-	has_many :events
-	# has_and_belongs_to_many :events, join_table: "attends"
-	# has_many :attends
-	# has_many :attending, through: :attends, source: "Event"
-	has_many :attending, class_name: "Event", join_table: "events"
-	has_many :votes, class_name: "Vote", join_table: "votes"
-
-	# has_many :events, through: :attends, as: :attending
-	# belongs_to :events, inverse_of: :attending
-	# has_many :events, through: :votes, as: :votes
 	has_many :tickets
+	has_many :events
+
+	has_and_belongs_to_many :attending, class_name: "Event", join_table: "attends"
+	has_and_belongs_to_many :votes, class_name: "Event", join_table: "votes"
 
 end
