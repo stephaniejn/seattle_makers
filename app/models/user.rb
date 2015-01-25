b59eaad528f787e82d96d0da37527e232079825b
Nick Lathe <nicklathe@icloud.com>
2015-01-25 14:33:45 -0800

    Added sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
 
+  has_secure_password
+
 	has_many :tickets
 	has_many :events
 
 	has_and_belongs_to_many :attending, class_name: "Event", join_table: "attends"
 	has_and_belongs_to_many :votes, class_name: "Event", join_table: "votes"
+
+  def self.authenticate email, password
+    User.find_by_email(email).try(:authenticate, password)
+  end
 
 end
