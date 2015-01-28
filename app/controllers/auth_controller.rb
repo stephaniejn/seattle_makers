01da8142a048547f0fe02f8ab8ca718051a21504
stephaniejn <stephaniejnilles@gmail.com>
2015-01-28 11:44:19 -0800

    Added Facebook API

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,27 +1,20 @@
 class AuthController < ApplicationController
 
-    def callback
-        @user = current_user
+  def callback
+    @user = current_user
 
-        if @user
-
-         uid =request.env['omniauth.auth']['uid']
-         @user.update_attribute("uid", uid)
-        end
-
-        # @user = User.find_or_create_by(auth_hash: provider_user['uid'], provider:params[:provider])
-
-        # if provider_user['info'] && provider_user['info']['name']
-        #     @user.name=provider_user['info']['name']
-        #     @user.save
-        # end
-        # render json: uid
-        redirect_to @user
+    if @user
+      uid =request.env['omniauth.auth']['uid']
+      @user.update_attribute("uid", uid)
     end
 
-    def failure
-        flash[:danger] = "An error occured - please try to upload your photo again"
-        redirect_to @user
-    end
+    redirect_to @user
+
+  end
+
+  def failure
+    flash[:danger] = "An error occured - please try to upload your photo again"
+    redirect_to @user
+  end
 
 end
