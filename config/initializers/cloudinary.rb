17f14dcdcddfb2d3da58946dd53e1b94a9681530
CestLou <alisonrebeccarobb@gmail.com>
2015-01-29 15:43:08 -0800

    Added unique cloudinary information

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,5 @@
 Cloudinary.config do |config|
-  config.cloud_name = 'dunfca9xv'
+  config.cloud_name = 'seattle-makers'
   config.api_key = ENV['CLOUDINARY_KEY']
   config.api_secret = ENV['CLOUDINARY_SECRET']
 end
