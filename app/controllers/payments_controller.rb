a7f99a28b76c9090f4d1b852f9a94a191ac77718
CestLou <alisonrebeccarobb@gmail.com>
2015-01-29 20:47:37 -0800

    venmo

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,7 +8,7 @@ class PaymentsController < ApplicationController
 
 	#Callback from venmo
 	def venmo_callback
-		puts "@"*99
+
 	  @venmo_user = Venmo.authenticate params[:code]
 	  if params[:state]
 		  state = params[:state].split("_")
@@ -17,8 +17,8 @@ class PaymentsController < ApplicationController
 			  to_user = User.find_by_id(state[0])
 			  to_email = to_user.email
 			  payment = @venmo_user.make_payment ({:email=>to_email, :note => 'Donation for your event on Seattle Trading Post!', :amount => state[1]})
-			  # binding.pry
-			  # puts payment.inspect
+			  binding.pry
+
 			  # render json: {venmo_user:@venmo_user, params:params, payment: payment, a}
 
 			  if payment.data && payment.data[:payment] && payment.data[:payment][:status] == "settled"
@@ -26,14 +26,16 @@ class PaymentsController < ApplicationController
 			  else
 			  	flash[:error] = "We were unable to process your payment."
 			  end
-				else
-					flash[:error] = "Couldnt find the user... Token probably expired."
-				end
-				redirect_to event_path(state[2])
 			else
-					flash[:error] = "Invalid state data."
-					redirect_to root_path
+				flash[:error] = "Couldnt find the user... Token probably expired."
 			end
-
+			redirect_to event_path(state[2])
+		else
+				flash[:error] = "Invalid state data."
+				redirect_to root_path
+		end
 	end
+
+
+
 end
