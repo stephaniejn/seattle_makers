cd991ec7c19c86b002b1e5a9556ee7db2e403b34
stephaniejn <stephaniejnilles@gmail.com>
2015-01-30 09:05:03 -0800

    Updated Trending to filter out full events

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'].to_i, ENV['FACEBOOK_SECRET']
-  env = ENV['RAILS_ENV'] || 'development'
-  if env=='production'
-    OmniAuth.config.full_host = 'https://seattletradingpost.herokuapp.com/auth/facebook/callback'
-  end
+  # env = ENV['RAILS_ENV'] || 'development'
+  # if env=='production'
+  #   OmniAuth.config.full_host = 'https://seattletradingpost.herokuapp.com/auth/facebook/callback'
+  # end
 end
